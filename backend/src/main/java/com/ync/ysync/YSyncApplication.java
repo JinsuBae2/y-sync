6efd5b24ceb394a3f9c2b743912d0cecdd0d3168
JinsuBae2 <bjs7898@naver.com>
2026-03-13 13:03:08 +0900

    chore: initial project setup (Spring Boot & Flutter)

---
+++ backend/src/main/java/com/ync/ysync/YSyncApplication.java
@@ -0,0 +1,13 @@
+package com.ync.ysync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YSyncApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ync/ysync/YSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ync.ysync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
